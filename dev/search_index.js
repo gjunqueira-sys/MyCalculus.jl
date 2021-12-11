8ba08127414ddbee578b408ea30c5e421a3f040f
Documenter.jl <documenter@juliadocs.github.io>
2021-12-11 14:35:42 +0000

    build based on bf313ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyCalculus","category":"page"},{"location":"#MyCalculus","page":"Home","title":"MyCalculus","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyCalculus.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyCalculus]","category":"page"}]
+}
